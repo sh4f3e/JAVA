6c1f141faa7ef0c5eb917ee392a9a73fb9e70e27
shafee <shafeess46@gmail.com>
2022-10-20 22:20:35 +0530

    new commit update in main java

---
+++ Main/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int a=10,b=20;
+        System.out.println(a+b);
+        System.out.print("This is shafee");
+    }
+}
